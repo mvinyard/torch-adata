389de7f2c6cbd4797c45177363e91eed517ab8a3
Michael Vinyard <47393421+mvinyard@users.noreply.github.com>
2022-10-15 15:28:15 -0400

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -51,6 +51,9 @@ extensions = ['autoapi.extension']
 autoapi_dirs = ['../../torch_adata']
 autosummary_generate = True
 
+html_static_path = ['_static']
+templates_path = ["_templates"]
+
 
 # -- Options for HTML output -------------------------------------------------------------
 
@@ -78,4 +81,4 @@ html_theme_options = {
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-# html_static_path = ['_static']
+
